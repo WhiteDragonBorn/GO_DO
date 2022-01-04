3924f354c417906110688dcfdcc60a05f429d3ac
WhiteDragonBorn <shimanoee@yandex.ru>
2022-01-04 23:03:12 +0300

    Update MAIN.gd
    Добавил transparency для смены цветов

--- Scripts/MAIN.gd
+++ Scripts/MAIN.gd
@@ -6,36 +6,36 @@ onready var BGC = get_node("Track/CCC")
 onready var SCORE = get_node("Track")
 onready var BTNS = [get_node("Button1"), 
 					get_node("Button2"),
-					get_node("Button3")] # Все кнопки помещены в список, чтобы
-										 # чтобы их легче было перебирать вместе
+					get_node("Button3")] # Кнопки помещены в список, чтобы
+										 # их легче было перебирать все сразу 
 
 onready var TURN = get_node("Turn") # Узел ходов
 var turn_num = 1					# Счётчик ходов
 
-func _ready():
+func _ready(): # Загрузка игры
 	rng.randomize()	# Обновляем наш рандомайзер (иначе одно и то же выдаёт)
 	var randint = rng.randi_range(1,20)
 	SCORE.set_text(str(randint)) # Главный счётчик игры (куча камней)
 
 func _on_Button_pressed():
-	BGC.color = Color(rng.randf(), rng.randf(), rng.randf())
+	BGC.color = Color(rng.randf(), rng.randf(), rng.randf(),0.5)
 	var num = int(SCORE.get_text()) + int(BTNS[1-1].get_text()[1])
 	SCORE.set_text(str(num)) # Обновляем кучу на значение btn
 	turn_num+=1 # Для каждой кнопки, чтобы считать кто ходит
 
 func _on_Button2_pressed():
-	BGC.color = Color(rng.randf(), rng.randf(), rng.randf())
+	BGC.color = Color(rng.randf(), rng.randf(), rng.randf(),0.5)
 	var num = int(SCORE.get_text()) + int(BTNS[2-1].get_text()[1])
 	SCORE.set_text(str(num))
 	turn_num+=1
 	
 func _on_Button3_pressed():
-	BGC.color = Color(rng.randf(), rng.randf(), rng.randf())
+	BGC.color = Color(rng.randf(), rng.randf(), rng.randf(),0.5)
 	var num = int(SCORE.get_text()) + int(BTNS[3-1].get_text()[1])
 	SCORE.set_text(str(num))
 	turn_num+=1
 	
-func _process(_delta):
+func _process(_delta): # Функция real-time, вызывается каждый frame
 	if turn_num % 2 != 0:         # Чётные - Ваня, Нечёт - Петя
 		TURN.text = "Petya's Turn"
 	else:
